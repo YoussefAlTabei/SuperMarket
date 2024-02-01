0d1230e5fd157fc8abbe90f6e44a6d8d73bac044
Asus <Asus@TABEIS-PC>
2024-02-01 18:30:57 +0200

    Add project files.

---
+++ SuperMarket/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMarket
+{
+    public partial class Form1 : Form
+    {
+        ProductDAL dal = new ProductDAL("server = localhost; database=market;uid=root;password=TabeiyoussEf9;");
+        Dictionary<Product, int> product = new Dictionary<Product, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+             product.Add( dal.SearchProductsByName(txtBarcodeSearch.Text),1);
+
+            // Iterate over the list of products and perform operations
+            foreach (Product product in products)
+            {
+                // Perform operations with the product object
+               label1.Text += ($"Barcode: {txtBarcodeSearch.Text}, Product Name: {product.Name}, Price: {product.Price}, Quantity: {product.QuantityInStock} \n");
+            }
+            Console.WriteLine(products.Count());
+        }
+
+        private void txtBarcodeSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SuperMarket/Products.cs
@@ -0,0 +1,71 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Barcode { get; set; }
+        public string Category { get; set; }
+        public int QuantityInStock { get; set; }
+        
+
+
+    }
+    public class ProductDAL
+    {
+        private string connectionString;
+        
+        public ProductDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Product> SearchProductsByName(string barcode)
+        {
+
+
+            List<Product> products = new List<Product>();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = $"SELECT * FROM Products WHERE Barcode = '{barcode}'";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+
+                    connection.Open();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                ProductID = Convert.ToInt32(reader["ProductID"]),
+                                Name = reader["Name"].ToString(),
+                                Price = Convert.ToDecimal(reader["Price"]),
+                                Barcode = reader["Barcode"].ToString(),
+                                Category = reader["Category"].ToString(),
+                                QuantityInStock = Convert.ToInt32(reader["QuantityInStock"])
+                                
+                            };
+                            products.Add(product);
+                            
+                        }
+                    }
+                }
+            }
+
+            return products;
+        }
+    }
+
+
+}
